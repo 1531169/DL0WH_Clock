8fbd2082461ce43af29878751d9f116f3962dfda
Cedric <Cedric@141.19.164.119>
2016-03-30 14:23:40 +0200

    added Main class

---
+++ DL0WH_Clock/src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+
+	public Main() {
+		// Initialize Clock
+	}
+
+	public static void main(String[] args) {
+		Main m = new Main();
+	}
+}
